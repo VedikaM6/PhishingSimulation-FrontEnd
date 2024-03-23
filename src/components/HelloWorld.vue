4b35d70bb8204a9f0d38726fbd449c97fbd7846b
Vedika Maheshwari <vmah860@gmail.com>
2024-03-23 14:15:35 -0400

    Changed the custom email template type to a drop down menu with options for user to select from

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -133,7 +133,8 @@
               <b-modal id="modal-2" title="New Email Template">
                 <div class="d-flex mb-3">
                   <label class="me-2 align-self-center w-20">Type: </label>
-                  <b-form-input type="text" class="mx-2"></b-form-input></br>
+                  <b-form-select v-model="newTemplateTypeSelected" :options="emailTypeOptions"
+                    class="form-select mx-2"></b-form-select></br>
                 </div>
                 <div class="d-flex mb-3">
                   <label class="me-2 align-self-center w-20">Company: </label>
@@ -154,7 +155,7 @@
           <!-- Right Side -->
           <!-- Displays more detail about the email template selected by user -->
           <div class="w-50 mx-2 px-2" v-show="isVisibleEmail">
-            <b-card :title=selectedAttack.name>
+            <b-card>
               <b-card-text>
                 <div>
                   <b-table stacked :items="[selectedEmailTemplate]">
@@ -183,6 +184,7 @@ export default {
       text: 'temp text - change later',
       attackLaterDate: '',
       attacklaterTime: '',
+      newTemplateTypeSelected: null,
       prevAttackArray: [
         { name: 'Attack 1', description: 'Content for Item 1', date: '01-01-2024', employees: [{ first_name: 'Dickerson', last_name: 'Macdonald', email: 'oldMcDonald@gmail.com' }, { first_name: 'Jami', last_name: 'Carney', email: 'JamiJami@gmail.com' }] },
         { name: 'Attack no. 2', description: 'Content for Item 2', date: '04-29-1990', employees: [{ first_name: 'Geneva', last_name: 'Wilson', email: 'Geneva@gmail.com' }] },
@@ -194,6 +196,7 @@ export default {
         { isSelected: false, first_name: 'Geneva', last_name: 'Wilson', email: 'Geneva@gmail.com' },
         { isSelected: false, first_name: 'Jami', last_name: 'Carney', email: 'JamiJami@gmail.com' }
       ],
+      // Attack Object that was selected by the user from the table
       selectedAttack: {
         name: '',
         description: '',
@@ -209,15 +212,24 @@ export default {
       emailTemplates: [
         {
           subject: 'Urgent Action Required: Your Walmart Account Security Alert',
-          body: 'Dear valued customer,\ntemp', type: 'Account Security', company: 'Walmart'
+          body: 'Dear valued customer,\ntemp', type: 'Account Security Alerts', company: 'Walmart'
         },
         {
           subject: 'New Deals - Template 2 Attack Email',
-          body: 'Dear valued customer,\nMore text\nMore and more text', type: 'Deals', company: 'Winners'
+          body: 'Dear valued customer,\nMore text\nMore and more text', type: 'Shopping Deals/Clearence Items', company: 'Winners'
         },
         {
-          subject: 'subject1 - email template', body: 'sdksadnlnclknakl', type: '', company: ''
+          subject: 'subject1 - email template', body: 'sdksadnlnclknakl', type: 'Financial', company: 'temp companyy'
         }
+      ],
+      emailTypeOptions: [
+        { value: 'Account Security Alerts', text: 'Account Security Alerts' },
+        { value: 'Shopping Deals/Clearence Items', text: 'Shopping Deals/Clearence Items' },
+        { value: 'Shipping Notifications', text: 'Shipping Notifications' },
+        { value: 'Financial', text: 'Financial' },
+        { value: 'Tech Support', text: 'Tech Support' },
+        { value: 'Surveys/Feedback', text: 'Surveys/Feedback' },
+        { value: 'Job Opportunities', text: 'Job Opportunities' }
       ]
     }
   },
