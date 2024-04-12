7d92d1b8878719cd7787e5d2fbb56c3361398e2d
Vedika Maheshwari <vmah860@gmail.com>
2024-04-11 21:18:27 -0400

    show data for first 3 gauge types

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -18,11 +18,27 @@
     <div class="rows-container flex-grow-1">
       <!-- Row 1 -->
       <div class="d-flex h-50 gauge-row">
-        <!-- Gauge 1: -->
+        <!-- Gauge 1: UserCounts -->
         <div class="gauge-container">
           <!-- Gauge content -->
           <div class="m-3">
+            <h4 class="gauge-title">User Counts</h4>
 
+            <span
+              v-if="allGaugeDataMap.UserCounts && allGaugeDataMap.UserCounts.data && allGaugeDataMap.UserCounts.data.length > 0">
+              <ul class="list-style-none ps-0">
+                <li v-for="user in allGaugeDataMap.UserCounts.data" class="d-flex justify-content-between">
+                  <!-- User name -->
+                  <p class="ms-3">{{ user.userName }}</p>
+
+                  <!-- User stats -->
+                  <p class="me-3">{{ user.numAttacksPassed + '/' + user.totalAttacks }}</p>
+                </li>
+              </ul>
+            </span>
+            <span v-else>
+              ...
+            </span>
           </div>
         </div>
 
@@ -30,7 +46,22 @@
         <div class="gauge-container">
           <!-- Gauge content -->
           <div class="m-3">
+            <h4 class="gauge-title">Overall Team Performance</h4>
 
+            <span v-if="allGaugeDataMap.TotalTeamPerformance && allGaugeDataMap.TotalTeamPerformance.data">
+              <!-- Show percentage of attacks passed-->
+              <h2>{{
+                roundNum((allGaugeDataMap.TotalTeamPerformance.data.numAttacksPassed /
+                  allGaugeDataMap.TotalTeamPerformance.data.totalAttacks) * 100)
+              }}%</h2>
+
+              <!-- Show counts of attacks passed-->
+              <p class="me-3">{{ allGaugeDataMap.TotalTeamPerformance.data.numAttacksPassed + '/' +
+                allGaugeDataMap.TotalTeamPerformance.data.totalAttacks }} attacks passed</p>
+            </span>
+            <span v-else>
+              ...
+            </span>
           </div>
         </div>
 
@@ -38,7 +69,23 @@
         <div class="gauge-container">
           <!-- Gauge content -->
           <div class="m-3">
+            <h4 class="gauge-title">Email Counts</h4>
 
+            <span
+              v-if="allGaugeDataMap.EmailCounts && allGaugeDataMap.EmailCounts.data && allGaugeDataMap.EmailCounts.data.length > 0">
+              <ul class="list-style-none ps-0">
+                <li v-for="email in allGaugeDataMap.EmailCounts.data" class="d-flex justify-content-between">
+                  <!-- Email name -->
+                  <p class="ms-3">{{ email.emailName }}</p>
+
+                  <!-- Email stats -->
+                  <p class="me-3">{{ email.numAttacksPassed + '/' + email.totalAttacks }}</p>
+                </li>
+              </ul>
+            </span>
+            <span v-else>
+              ...
+            </span>
           </div>
         </div>
       </div>
@@ -48,24 +95,24 @@
         <!-- Gauge 1: -->
         <div class="gauge-container">
           <!-- Gauge content -->
-          <div class="m-3">
-
+          <div class="gauge-content m-3">
+            {{ allGaugeDataMap.Gauge4 ? allGaugeDataMap.Gauge4.data : "..." }}
           </div>
         </div>
 
         <!-- Gauge 2: -->
         <div class="gauge-container">
           <!-- Gauge content -->
-          <div class="m-3">
-
+          <div class="d-inline-block m-3">
+            {{ allGaugeDataMap.Gauge4 ? allGaugeDataMap.Gauge4.data : "..." }}
           </div>
         </div>
 
         <!-- Gauge 3: -->
         <div class="gauge-container">
           <!-- Gauge content -->
-          <div class="m-3">
-
+          <div class="gauge-content m-3">
+            {{ allGaugeDataMap.Gauge4 ? allGaugeDataMap.Gauge4.data : "..." }}
           </div>
         </div>
       </div>
@@ -74,6 +121,12 @@
 </template>
 
 <script>
+import axios from 'axios';
+
+let api = axios.create({
+  baseURL: 'http://localhost:80'
+})
+
 export default {
   name: 'Dashboard',
   props: {
@@ -81,17 +134,76 @@ export default {
   },
   data() {
     return {
+      refreshDataInterval: null,
+
+      // this array contains the data for all the gauges
+      allGaugeDataArr: [],
+
+      // this object contains a property for each gauge type
+      //allGaugeDataMap: {}
 
     }
+  },
+  created() {
+    // load the data for the gauges
+    this.getDashboardData();
+
+    this.refreshDataInterval = setInterval(function () {
+      // refresh the data for the gauges every 30 seconds
+      this.getDashboardData();
+    }, 30000);
   },
   watch: {
 
   },
+  beforeUnmount() {
+    clearInterval(this.refreshDataInterval)
+  },
   computed: {
+    /* This computed object contains a property for each gauge type. */
+    allGaugeDataMap: function () {
+      let obj = {};
 
+      // loop through all gauges
+      for (let i = 0; i < this.allGaugeDataArr.length; i++) {
+        // add an entry for this gauge type
+        obj[this.allGaugeDataArr[i].type] = this.allGaugeDataArr[i];
+      }
+
+      return obj;
+    }
   },
   methods: {
+    getDashboardData() {
+      let config = {
+        headers: {
 
+        }
+      }
+      api.get("/dashboard/data", config)
+        .then(resp => {
+          // Handle the response
+          // NOTE: This is only executed when the request was successful
+          console.log("[getDashboardData] Got data!");
+          let responseData = resp.data;
+
+          // copy the data to 'allGaugeDataArr'
+          this.allGaugeDataArr = responseData.allGaugeData;
+
+        })
+        .catch(e => {
+          // NOTE: This is executed when an error occurs (non-200 response)
+          if (e.response && e.response.data) {
+            console.log(e.response.data);
+          } else {
+            console.log(e);
+          }
+        });
+    },
+    /* Round the given number to 2 decimal places */
+    roundNum(n) {
+      return Math.round((n * 100) / 100);
+    }
   }
 }
 </script>
@@ -105,13 +217,16 @@ export default {
 
 .gauge-container {
   border: 1px solid black;
-  flex-grow: 1;
+  flex: 1;
   height: 100%;
+  overflow-y: hidden;
+  text-align: center;
+  vertical-align: middle;
 }
 
 .gauge-container>div {
   border: 1px solid red;
-  height: 90%;
+  vertical-align: middle;
 }
 
 .page-container {
@@ -131,5 +246,13 @@ export default {
 
 .page-container .rows-container {
   flex: 1 1 auto;
+}
+
+.gauge-title {
+  font-weight: bold;
+}
+
+.list-style-none {
+  list-style: none;
 }
 </style>
